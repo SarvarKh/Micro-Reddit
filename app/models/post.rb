283e99c18aacc1244aabb9ad507e8dcdf4b95756
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-01-29 15:15:54 +0500

    Add associations between comments, users, posts and migrate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user, foreign_key: "user_id"
+    has_many :comments
 
     validates :title, uniqueness: true, length: { minimum: 3 }, presence: true
     validates :description, uniqueness: true, length: { minimum: 15 }, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :posts
-    
+    has_many :comments
+
     validates :name, length: { minimum: 2 }, uniqueness: true, presence: true
 end
