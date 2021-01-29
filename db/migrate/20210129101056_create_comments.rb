3c974fe8a64d03ee3409f786ef6c833c933b7280
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-01-29 15:22:43 +0500

    Correct migrations of comments, users, posts

--- db/migrate/20210129101056_create_comments.rb
+++ db/migrate/20210129101056_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.text, :content
-      t.integer, :user_id
+      t.text :content
+      t.integer :user_id
       t.integer :post_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_29_091019) do
+ActiveRecord::Schema.define(version: 2021_01_29_101056) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
