2d9f3ba16368c3e7772d9e7e9eaec072a9eb7b8a
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-01-29 14:20:45 +0500

    Add validations to the post models attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, uniqueness: true, length: { minimum: 3 }, presence: true
+    validates :description, uniqueness: true, length: { minimum: 15 }, presence: true
+    validates :user_id, uniqueness: true, presence: true, numericality: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_29_081058) do
+ActiveRecord::Schema.define(version: 2021_01_29_091019) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
