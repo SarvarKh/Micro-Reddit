29908c837b1b9b8ca982935e3cac171ea6365aaf
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-01-29 14:49:02 +0500

    Add associations to user and post models without migration yet

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+    belongs_to :user, foreign_key: "user_id"
+
     validates :title, uniqueness: true, length: { minimum: 3 }, presence: true
     validates :description, uniqueness: true, length: { minimum: 15 }, presence: true
     validates :user_id, uniqueness: true, presence: true, numericality: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+    has_many :posts
+    
     validates :name, length: { minimum: 2 }, uniqueness: true, presence: true
 end
